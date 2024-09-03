25bd7cc82dc58739d46e9133c7b13febc0faf7c4
viovar <168191154+viovar@users.noreply.github.com>
2024-09-03 17:25:59 +0300

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.views.generic.edit import CreateView
+from .forms import SignUpForm
+
+
+class SignUp(CreateView):
+    model = User
+    form_class = SignUpForm
+    success_url = '/accounts/login'
+    template_name = 'registration/signup.html'
+
+
+# Create your views here.
